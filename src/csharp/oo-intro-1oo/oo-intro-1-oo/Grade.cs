94bdab654a97ee3217bc5caa71cacbd5d17c4094
Jonathan Melly <jonathan.melly@gmail.com>
2018-07-03 16:27:12 +0200

    ajouté début de version oo

---
+++ src/csharp/oo-intro-1oo/oo-intro-1-oo/Grade.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace object_oriented_introduction
+{
+    public class Grade
+    {
+        //Pour stocker la valeur de la note (entre 0 et 6)
+        private float value;
+
+
+        public Grade(float value)
+        {
+            Value = value;
+        }
+
+        public float Value
+        {
+            get
+            {
+                return value;
+            }
+            set
+            {
+                //TODO: vérifier que value est bien entre 0 et 6 ...
+                this.value = value;
+            }
+        }
+    }
+}
---
+++ src/csharp/oo-intro-1oo/oo-intro-1-oo/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace object_oriented_introduction
+{
+    class Student
+    {
+    }
+}
